04bd8976dae0abcd886af4f6546b7beda346985a
mathias kamp <mathias.kamp@hotmail.com>
2020-12-14 12:22:12 +0100

    øvelse 3 finished

--- ThreadProgramming/Program.cs
+++ ThreadProgramming/Program.cs
@@ -11,21 +11,22 @@ namespace ThreadProgramming
     {
         static void Main(string[] args)
         {
-            #region Øvelse 1
+            #region Øvelse 0
 
             //create new object of WorkThread class
             WorkThread wt = new WorkThread();
 
             //create new Thread and new ThreadStart that class WorkThreadFunction method from WorkThread class.
             Thread tFirst = new Thread(new ThreadStart(wt.WorkThreadFunction));
-            Thread tSecound = new Thread(new ThreadStart(wt.WorkThreadTwo));
-            // starts the Thread
-            tFirst.Start();   
-            tSecound.Start();
-            tFirst.Join();
-            tSecound.join();
-            Console.Read();
 
+            tFirst.Name = "Work thread 1";
+            // starts the Thread
+            tFirst.Start();
+            tFirst.Join();
+            Thread tSecound = new Thread(new ThreadStart(wt.WorkThreadTwo));
+            tSecound.Name = "Work thread 2";
+            tSecound.Start();
+            Console.Read();
             #endregion
         }
     }
